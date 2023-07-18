3766140f7651b781eb7ddff26ce60af99dc1a4fe
linus.schoeb <linus.schoeb@student.kit.edu>
2023-07-18 22:12:27 +0200

    Implement maximum Uuid

--- src/ast/uuid_provider.rs
+++ src/ast/uuid_provider.rs
@@ -2,6 +2,9 @@ use serde::Serialize;
 
 pub type Uuid = u64;
 
+static JS_MAX_SAFE_INTEGER: Uuid = 2u64.pow(53);
+static MAX_UUID: Uuid = JS_MAX_SAFE_INTEGER;
+
 pub trait UuidProvider {
     fn new_uuid(&mut self) -> Uuid;
     // TODO use Option<Uuid> as return type as in spec?
@@ -15,6 +18,10 @@ pub struct TexlaUuidProvider {
 impl UuidProvider for TexlaUuidProvider {
     fn new_uuid(&mut self) -> Uuid {
         self.highest_uuid += 1;
+        if self.highest_uuid > MAX_UUID {
+            // we do not expect to have this many nodes (2^53 = 9e15)
+            panic!("UUID overflow")
+        }
         self.highest_uuid
     }
 }
