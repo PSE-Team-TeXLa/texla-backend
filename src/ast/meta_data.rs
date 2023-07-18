9c48fc158a328cfb8e210db79363b8fc60134a59
linus.schoeb <linus.schoeb@student.kit.edu>
2023-07-18 11:51:50 +0200

    Implement meta data

--- src/ast/meta_data.rs
+++ src/ast/meta_data.rs
@@ -8,6 +8,7 @@ pub struct MetaData {
     pub(crate) data: HashMap<String, String>,
 }
 
+// TODO: maybe &str is sufficient here
 impl MetaData {
     fn new() -> Self {
         Self {
@@ -16,10 +17,12 @@ impl MetaData {
     }
 
     fn edit_meta_data(&mut self, new_meta_data: HashMap<String, String>) {
-        todo!()
+        for (key, value) in new_meta_data {
+            self.data.insert(key, value);
+        }
     }
 
     fn delete_meta_data(&mut self, key: String) {
-        todo!()
+        self.data.remove(&key);
     }
 }
