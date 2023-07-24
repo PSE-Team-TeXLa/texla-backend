df5e9aa8a6ca0a7c2eac840902857b196c038c46
linus.schoeb <linus.schoeb@student.kit.edu>
2023-07-24 21:13:48 +0200

    Remove unused level_of_children

--- src/ast/operation/move_node.rs
+++ src/ast/operation/move_node.rs
@@ -63,25 +63,3 @@ impl Operation<TexlaAst> for MoveNode {
         Ok(())
     }
 }
-
-// we will probably never need this function :(
-// TODO: replace u8 with i8 (when merging with parser)
-fn level_of_children(ast: &TexlaAst, uuid: Uuid) -> u8 {
-    let node_ref = ast.get_node(uuid);
-    let node = node_ref.lock().unwrap();
-    match node.parent.as_ref() {
-        None => ast.highest_level, // node is root
-        Some(parent_ref_weak) => {
-            let parent_ref = parent_ref_weak.upgrade().unwrap();
-            let parent = parent_ref.lock().unwrap();
-            match &node.node_type {
-                // only segments count to the level depth
-                NodeType::Expandable { data, .. } => match data {
-                    ExpandableData::Segment { .. } => level_of_children(ast, parent.uuid) + 1,
-                    _ => level_of_children(ast, parent.uuid),
-                },
-                NodeType::Leaf { .. } => panic!("parent is a leaf"),
-            }
-        }
-    }
-}
