2132ca391c1be1a908dd21ed6047cdd66eb391a8
linus.schoeb <linus.schoeb@student.kit.edu>
2023-07-28 19:56:48 +0200

    Serve user assets dynamically

--- src/texla/webserver.rs
+++ src/texla/webserver.rs
@@ -1,11 +1,13 @@
-use axum::http::{header, Request, Response, StatusCode};
-use axum::response::{Html, IntoResponse};
-use axum::routing::get;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::sync::{Arc, RwLock};
 
+use axum;
 use axum::body::StreamBody;
-use axum::{Error, Server};
+use axum::handler::Handler;
+use axum::http::{header, StatusCode};
+use axum::response::IntoResponse;
+use axum::routing::get;
+use axum::{Extension, Server};
 use tower_http::services::{ServeDir, ServeFile};
 use tower_http::set_status::SetStatus;
 use tower_http::trace::TraceLayer;
@@ -18,13 +20,13 @@ const FRONTEND_PATH: &str = "frontend";
 
 pub async fn start_axum(core: Arc<RwLock<TexlaCore>>) {
     let app = axum::Router::new()
-        // .route("/dummy", get(|| async { Html("This is a dummy file.") }))
         .fallback_service(static_files())
         .route("/user-assets/*path", get(user_assets_handler))
         .layer(
             TraceLayer::new_for_http().on_body_chunk(()).on_eos(()), // .on_request(log_request)
         )
-        .layer(socket_service(core));
+        .layer(socket_service(core.clone()))
+        .layer(Extension(core.clone()));
 
     let res = Server::bind(&([127, 0, 0, 1], PORT).into())
         .serve(app.into_make_service())
@@ -49,30 +51,38 @@ fn static_files() -> ServeDir<SetStatus<ServeFile>> {
 }
 
 async fn user_assets_handler(
+    Extension(core): Extension<Arc<RwLock<TexlaCore>>>,
     axum::extract::Path(path): axum::extract::Path<String>,
-) -> Result<axum::response::Response, StatusCode> {
-    println!("Serving user file: {}", path);
-    let main_file = "latex_test_files/lots_of_features.tex";
-    let main_file_path = PathBuf::from(main_file)
-        .canonicalize()
-        .expect("Could not find main file directory");
-    let main_file_directory = main_file_path
-        .parent()
-        .expect("main_file cannot be a root directory");
+) -> Result<impl IntoResponse, StatusCode> {
+    println!("Serving user asset: {}", path);
+
+    let main_file_directory = {
+        let main_file = &core.read().unwrap().main_file;
+        let main_file_path = PathBuf::from(main_file)
+            .canonicalize()
+            .expect("Could not find main file directory");
+        main_file_path
+            .parent()
+            .expect("main_file cannot be a root directory")
+            .to_path_buf()
+    };
 
     let file = match tokio::fs::File::open(main_file_directory.join(&path)).await {
         Ok(file) => file,
-        Err(err) => return Err(StatusCode::BAD_REQUEST),
+        Err(err) => return Err(StatusCode::IM_A_TEAPOT),
     };
     // convert the `AsyncRead` into a `Stream`
     let stream = tokio_util::io::ReaderStream::new(file);
     // convert the `Stream` into an `axum::body::HttpBody`
     let body = StreamBody::new(stream);
 
-    let header = format!("attachment; filename=\"{}\"", path);
+    let content_disposition_header = format!("attachment; filename=\"{}\"", path);
     let headers = [
         (header::CONTENT_TYPE, "text/toml; charset=utf-8"),
-        (header::CONTENT_DISPOSITION, header.as_str()),
+        (
+            header::CONTENT_DISPOSITION,
+            content_disposition_header.as_str(),
+        ),
     ];
 
     Ok((headers, body).into_response())
