bd6e3065c91f316872d2d585fb605cad4e1cbe12
piotr.malkowski <piotr.malkowski@student.kit.edu>
2023-07-28 17:34:02 +0200

    Replace Stream with simpler code, make it work.

--- src/infrastructure/export_manager.rs
+++ src/infrastructure/export_manager.rs
@@ -29,32 +29,69 @@ impl ExportManager for TexlaExportManager {
             .to_path_buf();
 
         //"http://127.0.0.1:3002/src/lib/assets/logo/latex.zip"
-        let file = File::create("latex.zip").unwrap(); // change zip location to frontend structure. TODO Linus fragen
+        let file = File::create("/home/piotr/CLionProjects/backend/latex.zip").unwrap(); // change zip location to frontend structure. TODO Linus fragen
+
         let option = FileOptions::default()
             .compression_method(Deflated) // default zip method.
             .unix_permissions(0o755); //shouldn't cause any errors in windows, should work on linux and mac.
+
         let mut zip = zip::ZipWriter::new(file);
 
-        let walkdir = walkdir::WalkDir::new(main_file_directory).into_iter();
-        walkdir
-            .filter_map(|e| e.ok()) // filtering (ignoring) invalid files errors
-            .filter(|e| {
-                let name = e.file_name().to_string_lossy();
-                !name.starts_with('.') && !name.ends_with('~') // filtering out hidden files or backup files. Good practice apparently.
-            })
-            .for_each(|entry| {
-                let path = entry.path();
-                //let name = path.strip_prefix(path).unwrap(); //results in empty string, should strip base directory prefix
+        let walkdir = walkdir::WalkDir::new(main_file_directory.clone()).into_iter();
 
-                let mut f = File::open(path).unwrap();
-                zip.start_file(path.to_str().unwrap(), option).unwrap();
+        //walkdir
+        //    .filter_map(|e| e.ok()) // filtering (ignoring) invalid files errors
+        //    .filter(|e| {
+        //        let name = e.file_name().to_string_lossy();
+        //        !name.starts_with('.') && !name.ends_with('~') // filtering out hidden files or backup files. Good practice apparently.
+        //    })
+        //    .for_each(|entry| {
+        //        let path = entry.path();
+        //        //let name = path.strip_prefix(path).unwrap(); //results in empty string, should strip base directory prefix
+        //
+        //        let mut f = File::open(path).unwrap();
+        //        zip.start_file(path.to_str().unwrap(), option).unwrap();
+        //
+        //        let mut buffer = Vec::new(); // could be problematic if files are too big.
+        //        f.read_to_end(&mut buffer).unwrap();
+        //        zip.write_all(&buffer).unwrap();
+        //    });
 
-                let mut buffer = Vec::new(); // could be problematic if files are too big.
-                f.read_to_end(&mut buffer).unwrap();
-                zip.write_all(&buffer).unwrap();
-            });
+        for entry in walkdir {
+            let entry = entry.unwrap();
+            let path = entry.path();
+            if path.is_file() {
+                let name = path
+                    .strip_prefix(&main_file_directory)
+                    .unwrap()
+                    .to_str()
+                    .unwrap();
 
-        // TODO
-        Ok("http://127.0.0.1:3002/src/lib/assets/logo/graph.svg".to_string()) // TODO linus, wo soll das zip gespeichert werden?
+                if !name.starts_with('.') && !name.ends_with('~') {
+                    let mut f = File::open(path).unwrap();
+                    zip.start_file(name, option).unwrap();
+
+                    let mut buffer = Vec::new();
+                    f.read_to_end(&mut buffer).unwrap();
+                    zip.write_all(&buffer).unwrap();
+                }
+            }
+        }
+
+        Ok("http://127.0.0.1:3002/src/lib/assets/logo/latex.zip".to_string()) // TODO linus, wo soll das zip gespeichert werden?
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::infrastructure::export_manager::{ExportManager, TexlaExportManager};
+
+    #[test]
+    fn zip_files() {
+        let main_file = "latex_test_files/latex_with_inputs.tex".to_string();
+        let mut export_manager = TexlaExportManager::new(main_file);
+
+        let url = export_manager.zip_files();
+        let should_url = "http://127.0.0.1:3002/src/lib/assets/logo/latex.zip";
     }
 }
