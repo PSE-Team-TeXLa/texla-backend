99b0745f184001ca882e76e56bb3c90447a80776
piotr.malkowski <piotr.malkowski@student.kit.edu>
2023-08-10 21:55:27 +0200

    Refactor addNode test

--- ast/src/operation/add_node.rs
+++ ast/src/operation/add_node.rs
@@ -57,48 +57,54 @@ mod tests {
 
     #[test]
     fn test_add_node() {
-        let original_latex = fs::read_to_string("../test_resources/latex/simple.tex").unwrap();
-        let mut ast = parse_latex(original_latex.clone()).expect("Valid Latex");
+        let subsection_to_be_added_to_raw_latex = "\\subsection{Subtitle}";
+        let subsubsection_to_be_added_raw_latex = "\\subsubsection{Subsubtitle}";
 
-        let subsection_uuid =
-            find_uuid_by_content(&ast, "\\subsection{Subtitle}").expect("Failed to find");
+        let original_latex_single_string =
+            fs::read_to_string("../test_resources/latex/simple.tex").unwrap();
+        let mut ast = parse_latex(original_latex_single_string.clone()).expect("Valid Latex");
+
+        let subsection_uuid = find_uuid_by_content(&ast, subsection_to_be_added_to_raw_latex)
+            .expect("Failed to find");
 
         let position = Position {
             parent: subsection_uuid,
             after_sibling: None,
         };
 
-        // 1. Get the parent node and count its children before the operation
-        let parent_node_before = ast.get_node(subsection_uuid);
-
-        //let raw_latex_parent = parent_node_before.lock().unwrap().raw_latex.to_string();
-        //println!("raw latex: {}", raw_latex_parent);
-
-        let children_count_before = match &parent_node_before.lock().unwrap().node_type {
-            NodeType::Expandable { children, .. } => children.len(),
-            _ => panic!("Parent node should be of type Expandable"),
-        };
+        let subsection_children_count_before =
+            get_node_and_count_children(&ast, subsection_to_be_added_to_raw_latex);
 
         let operation = Box::new(AddNode {
             destination: position,
-            raw_latex: "".to_string(),
+            raw_latex: subsubsection_to_be_added_raw_latex.to_string(),
         });
 
         ast.execute(operation).expect("should succeed");
+        // reparse
+        let new_latex_single_string = ast.to_latex(Default::default()).unwrap();
+        ast = parse_latex(new_latex_single_string.clone()).expect("Valid Latex");
 
-        // 2. Get the parent node and count its children after the operation
-        let parent_node_after = ast.get_node(subsection_uuid);
-        let children_count_after = match &parent_node_after.lock().unwrap().node_type {
-            NodeType::Expandable { children, .. } => children.len(),
-            _ => panic!("Parent node should be of type Expandable"),
-        };
+        let subsection_children_count_after =
+            get_node_and_count_children(&ast, subsection_to_be_added_to_raw_latex);
+        let subsubsection_childer_count_after_adding =
+            get_node_and_count_children(&ast, subsubsection_to_be_added_raw_latex);
 
-        // 3. Verify
+        // subsection{subtitle} should now only have one child, namely subsubsection
+        // all the children from before should now be contained in subsubsection
         assert_eq!(
-            children_count_before + 1,
-            children_count_after,
+            subsection_children_count_before - 1,
+            subsection_children_count_after,
             "The parent node should have one more child after the operation"
         );
+
+        assert_eq!(
+            subsection_children_count_before, subsubsection_childer_count_after_adding,
+            "subsubsection should now contain the children of subsection"
+        );
+
+        assert!(!original_latex_single_string.contains(subsubsection_to_be_added_raw_latex));
+        assert!(new_latex_single_string.contains(subsubsection_to_be_added_raw_latex));
     }
 
     fn find_uuid_by_content(ast: &TexlaAst, content: &str) -> Option<Uuid> {
@@ -108,7 +114,6 @@ mod tests {
     fn find_uuid_by_content_recursive(node_ref: &NodeRef, content: &str) -> Option<Uuid> {
         let node = node_ref.lock().unwrap();
         let current_raw_latex = &node.raw_latex.to_string();
-        //println!("current_raw_latex: {}", current_raw_latex.to_string());
 
         // Check if the raw_latex of the current node matches the content
         if current_raw_latex.contains(content) {
@@ -132,47 +137,17 @@ mod tests {
 
         None
     }
-}
 
-//    #[test]
-//    fn test_add_node() {
-//        let original_latex = fs::read_to_string("../test_resources/latex/simple.tex").unwrap();
-//        let mut ast = parse_latex(original_latex.clone()).expect("Valid Latex");
-//
-//        let subsection_uuid =
-//            find_uuid_by_content(&ast, "\\subsection{Subtitle}").expect("Failed to find");
-//
-//        let raw_latex = "\\subsubsection{New Subsubsection}";
-//
-//        let position = Position {
-//            parent: subsection_uuid,
-//            after_sibling: None,
-//        };
-//
-//        let operation = Box::new(AddNode {
-//            destination: position,
-//            raw_latex: raw_latex.to_string(),
-//        });
-//
-//        //println!("raw_latex_in_operation: {}", operation.raw_latex);
-//
-//        ast.execute(operation).expect("should succeed");
-//
-//        let new_ast_to_latex = ast.to_latex(Default::default());
-//
-//        println!("new ast: {}", new_ast_to_latex.unwrap().to_string());
-//
-//        let new_sub_sub_section_uuid = find_uuid_by_content(&ast, raw_latex);
-//
-//        //let parent_node_uuid_from_subsubsection = ast
-//        //    .get_node(new_sub_sub_section_uuid.expect(""))
-//        //    .lock()
-//        //    .unwrap()
-//        //    .parent
-//        //    .as_ref()
-//        //    .expect("New subsubsection should have a parent")
-//        //    .upgrade()
-//        //    .expect("Parent node should be valid");
-//
-//        assert!(new_sub_sub_section_uuid.is_some(), "subsection added");
-//    }
+    fn get_node_and_count_children(ast: &TexlaAst, content: &str) -> usize {
+        let node_uuid = find_uuid_by_content(ast, content).expect("Failed to find");
+        let node_ref = ast.get_node(node_uuid);
+        count_children_of_node(&node_ref)
+    }
+
+    fn count_children_of_node(node_ref: &NodeRef) -> usize {
+        match &node_ref.lock().unwrap().node_type {
+            NodeType::Expandable { children, .. } => children.len(),
+            _ => 0, // Return 0 for non-Expandable nodes
+        }
+    }
+}
